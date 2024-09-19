2aac2b31880b86d4ff6a275dfb0b00bb541a5135
Kaylee-Baichulall-ICD2O1-01 <kaylee.baichulall@student.tdsb.on.ca>
2024-09-19 14:26:09 +0000

    commit one

--- PP1_4.py
+++ PP1_4.py
@@ -1,16 +1,27 @@
-
+'''
+Lesson 3 - Input, concatenation and placeholders
+Author: Kaylee Baichulall
+Date Created: September 19th, 2024
+Date Last Modified: September 19th, 2024
+'''
 
 def q1():
-  #Write Assignment code here
+  userWord = input("Input a word: ")
+  print(userWord)
 
 def q2():
-  #Write Assignment code here
+  userName = input("Input your first name: ")
+  print("Hello " + userName)
 
 def q3():
-  #Write Assignment code here
+  userFirstName = input("Input your first name: ")
+  userLastName = input("Input your last name: ")
+  print(f"{userFirstName} {userLastName}")
 
 def q4():
-  #Write Assignment code here
+  studentOne = input("Input a student: ")
+  studentTwo = input("Input another student: ")
+  print(f"Your students are {studentOne} and {studentTwo}")
 
 #Do not edit code below this comment
 
